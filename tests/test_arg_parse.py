85ce28b70bf418f157a57fc29b144fc799534823
DavidVentura <davidventura27@gmail.com>
2022-11-09 19:20:37 +0100

    add dashes test

--- tests/test_arg_parse.py
+++ tests/test_arg_parse.py
@@ -19,3 +19,8 @@ def test_args_to_kwargs2():
 def test_args_with_flag():
     res = args_to_kwargs(['--arg2', '20', '--activate'])
     assert res == {'arg2': '20', 'activate': None}
+
+
+def test_args_with_dash_become_underscore():
+    res = args_to_kwargs(['--with-dashes', '1'])
+    assert res == {'with_dashes': '1'}
